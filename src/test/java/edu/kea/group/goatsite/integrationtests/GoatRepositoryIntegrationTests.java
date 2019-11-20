48be34c0b5d32f94915c97a49db03fcd03c71061
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2019-11-20 13:02:48 +0100

    Added integration test for goat changing mechanism

--- src/test/java/edu/kea/group/goatsite/integrationtests/GoatRepositoryIntegrationTests.java
+++ src/test/java/edu/kea/group/goatsite/integrationtests/GoatRepositoryIntegrationTests.java
@@ -1,5 +1,6 @@
 package edu.kea.group.goatsite.integrationtests;
 
+import edu.kea.group.goatsite.model.Gender;
 import edu.kea.group.goatsite.model.Goat;
 import edu.kea.group.goatsite.repository.GoatRepository;
 import org.junit.Assert;
@@ -7,13 +8,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class GoatRepositoryIntegrationTests {
+
+    @Autowired
+    TestEntityManager entityManager;
 
     @Autowired
     GoatRepository goatRepository;
@@ -46,6 +53,39 @@ public class GoatRepositoryIntegrationTests {
             Goat returnGoat = returnGoats.iterator().next();
             Assert.assertEquals(goat, returnGoat);
         }
+
+    }
+
+    /* Integration test to see if we can change information about a current goat in our system */
+    @Test
+    public void canChangeGoatInformation() {
+
+        /* Prepare goat whose info we want to change */
+        // Create a goat
+        Goat goat = new Goat();
+        goat.setName("Ferdinand");
+        goat.setUsername("Ferdinand");
+        goat.setDob(new Date(2000));
+        goat.setEnabled(true);
+        goat.setGender(Gender.MALE);
+        goat.setLongDescription("Be Baaaaahd like me!");
+
+        // Add goat to persistent storage
+        goatRepository.save(goat);
+
+        // Save the long description as a string
+        String formerLongDescription = goat.getLongDescription();
+
+
+        /* Start to change information and save new entity to DB */
+        goat = goatRepository.findByUsername("Ferdinand");
+        goat.setLongDescription("Wanna be baaaaahd with me?!");
+        goatRepository.save(goat);
+
+
+        /* Assert if the information has been changed */
+        assertThat(formerLongDescription)
+                .isNotEqualTo(goatRepository.findByUsername("Ferdinand").getLongDescription());
 
     }
 
